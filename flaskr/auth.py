d43d670462195c64cf2b5770d417177fd6744f57
Erik Thiem <erikthiem@gmail.com>
2021-06-29 14:36:09 -0400

    reorganize

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -9,6 +9,27 @@ from flaskr.db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+def login_required(view):
+    @functools.wrap(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirecet(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
     if request.method == 'POST':
@@ -61,28 +82,7 @@ def login():
 
     return render_template('auth/login.html')
 
-@bp.before_app_request
-def load_logged_in_user():
-    user_id = session.get('user_id')
-
-    if user_id is None:
-        g.user = None
-    else:
-        g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id,)
-        ).fetchone()
-
 @bp.route('/logout')
 def logout():
     session.clear()
     return redirect(url_for('index'))
-
-def login_required(view):
-    @functools.wrap(view)
-    def wrapped_view(**kwargs):
-        if g.user is None:
-            return redirecet(url_for('auth.login'))
-
-        return view(**kwargs)
-
-    return wrapped_view
